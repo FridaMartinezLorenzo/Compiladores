42fd90ceb87fa4a632b8b8f518db11ac8b519786
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2023-11-09 22:17:29 -0600

    Se va a eliminar el split

--- Analizador_Lexico/main.py
+++ Analizador_Lexico/main.py
@@ -25,15 +25,19 @@ class element_TokenTable:
         self.nlinea = nl
 
     def __str__(self):
-        return self.lexema + " " + self.token
+        return self.lexema + " " + self.token+ " " + str(self.nlinea)
+    
 
 def file_breakdown (lines, tokenList):
     nline = 1
     for line in lines:
         words_list = line.split()
         for word in words_list:
-            word_search(word, nline, tokenList)
+            flag_found = word_search(word, nline, tokenList) 
+            if (flag_found == False):
+                not_word_search(word, nline, tokenList)
         nline += 1
+        #print("nline: ", nline)
 
 
 def word_search(word, nline, tokenList):
@@ -45,11 +49,29 @@ def word_search(word, nline, tokenList):
     
 def not_word_search(word, nline, tokenList):
     aux=""
+    flag_string = False
     for char in word:
         aux+=char
-        print(word_search(word, nline, tokenList))
-        if (flag == False):
-        #print(aux)
+        print("flag_string: ",flag_string)
+        print("aux: ",aux)
+        
+        if char == '"' and flag_string == True: #Se encontro el fin de la cadena
+            tokenList.append(element_TokenTable(aux, "varCadena", nline)) #Agregamos a la lista de tokens
+            aux=""
+            
+        if char == '"': #Es una cadena, se empieza a guardar y se enciende la bandera y asi si esta la bandera encendida esperamos el siguiente
+            flag_string = True
+            
+        if char in lista_simbolos and flag_string == False: #Es un simbolo
+            tokenList.append(element_TokenTable(char, char, nline)) #Agregamos a la lista de tokens
+            aux=""
+            
+        
+        if flag_string == False:
+            flag_found1 = word_search(aux, nline, tokenList)
+            if (flag_found1 == True):
+                aux=""
+        
     
         
     
@@ -63,6 +85,11 @@ with open(file, 'r') as f:
     lines_entry_file = f.readlines()
 
 tokenList = []
-#file_breakdown(lines_entry_file, tokenList)   
+file_breakdown(lines_entry_file, tokenList)   
+for token in tokenList:
+    print(token)
 
-not_word_search("String", 1, tokenList)
+#char = '"hola"'
+#if '"' == char:
+#    print("good")
+#not_word_search(char, 1, tokenList)
