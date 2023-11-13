f8a0d35cd7dc06f61e261825f2ca4cd4807136f8
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2023-11-13 01:17:27 -0600

    Se modificó el segundo archivo de prueba

--- Proyecto/prueba1.java
+++ Proyecto/prueba1.java
@@ -1,9 +1,10 @@
 public class prueba1 {
     public static void saludar() {
         System.out.println("Hola, mundo");
+        int a = 5;
     }
 
     public static void main(String[] args) {
-       
+       int b = 6;
     }
 }
