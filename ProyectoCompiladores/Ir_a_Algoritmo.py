47e0a3bf9571fe6112779bc6838559a991f4c0ad
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2023-11-25 00:02:23 -0600

    Se resolvió el error en Ir_a, falta corregir el bucle

--- ProyectoCompiladores/Ir_a_Algoritmo.py
+++ ProyectoCompiladores/Ir_a_Algoritmo.py
@@ -1,51 +1,94 @@
+from cerradura import *
 
-def Ir_a(conjuntoI , simboloAevaluar):
-    J = [] #Declaramos la tupla que se va a retornar    
-    
+from cerradura import *
+
+def generar_estado(estado, produccion):
+    return [estado, [produccion]]
+
+def Ir_a(conjuntoI, simboloAevaluar, reglas_prod):
+    J = []  # Declaramos la tupla que se va a retornar
+    print("Estamos trabajando en ir a")
+    estado_actual_I = str(conjuntoI[0])
+
     for elemento in conjuntoI[1]:
         if simboloAevaluar == '$':
             J.append("Aceptacion")
             return J
-        
-        
-        print("elemento: ",elemento)
-        base  = elemento[0] #Base de la regla de produccion
-        estados = elemento[1] #Estados de la regla de produccion
-        print("estados: ",estados)
-        temporal = [] #Lista temporal para guardar los estados
-        bandera_se_detecto_punto = bandera_se_encontro_simbolo =False
 
+        print("elemento: ", elemento)
+        base = elemento[0]  # Base de la regla de produccion
+        estados = elemento[1]  # Estados de la regla de produccion
+        print("estados: ", estados)
+        temporal = []  # Lista temporal para guardar los estados
+        bandera_se_detecto_punto = bandera_se_encontro_simbolo = False
+
+        for estado in estados:
+            if estado == simboloAevaluar:
+                bandera_se_encontro_simbolo = True
+
+            if estado == '•':
+                bandera_se_detecto_punto = True
+
+            if estado != '•':
+                temporal.append(estado)
+
+            if estado != '•' and bandera_se_detecto_punto == True:
+                temporal.append('•')
+                bandera_se_detecto_punto = False
+
+        if bandera_se_encontro_simbolo:
+            aux = [base, temporal]
+            J.append(aux)
+    # Imprimimos listas
+    print("\n\nLista de elementos a mandar a cerradura: ", J)
+    
+    for elemento in J:
+        print("elemento: ", elemento)
+    
+    
+    Lista_aux = []
+    for elemento in J:
+        print("elemento: ", elemento)
+        aux = []
+        aux.append(elemento[0])
+        aux_interno = []
+        for elemento2 in elemento[1]:
+            aux_interno.append(elemento2)
+            print("elemento2: ", elemento2)
+            
+        aux.append(aux_interno)
+        print("aux: ", aux)
+        Lista_aux.append(aux)
+    
         
-        for estado in estados: #Obtenemos el listado de estado
-            #Vamos a recorrer el punto de la produccion y copiamos en una variable temporal
-                if estado == simboloAevaluar:
-                    bandera_se_encontro_simbolo = True
-                    #print("Se encontro simbolo")
-                    
-                if estado == '•':
-                    bandera_se_detecto_punto = True
-                    #print("Se detecto punto")
-                    
-                if estado != '•':
-                    temporal.append(estado)
-                    
-                if estado != '•' and bandera_se_detecto_punto == True:
-                    temporal.append('•')
-                    bandera_se_detecto_punto = False
-                #print("temporal: ",temporal)
-        
-        if bandera_se_encontro_simbolo == True:        
-            J.append([base, [temporal]]) #Agregamos a la tupla
-  
+    print("\nLista_aux: ", Lista_aux)
+    
+    J = [estado_actual_I, Lista_aux]
+    
+    print("\nJ: ", J)
+    J = cerradura(J, reglas_prod)
     return J
 
 
-conjuntoI =           ['I0',  [       ['S', ['•', 'i', 'p'] ], ['E',['•','i','f']  ]      ]   ]     
-#print(conjuntoI)
-J = Ir_a(conjuntoI,'$')
 
+# Resto del código...
+
+
+
+'''
+
+conjuntoI =           ['I0',  [       ['S', ['•', 'i', 'p'] ], ['E',['•','i','f']  ]      ]   ]   , ['I1',  [       ['S', ['•', 'i', 'p'] ], ['E',['•','i','f']  ]      ]   ]     
+for e in conjuntoI:
+    print(e)
+    
+#conjuntoI = conjuntoI[1:]
+
+conjuntoI = list(conjuntoI)
+del conjuntoI[0]
 print(conjuntoI)
-print (J)
+# J = Ir_a(conjuntoI,'i')
 
-#K = Ir_a(J,'i')
-#print(K)
+#print(conjuntoI)
+#print (J)
+
+'''
--- ProyectoCompiladores/coleccionCanonica.py
+++ ProyectoCompiladores/coleccionCanonica.py
@@ -1,4 +1,7 @@
 
+from Ir_a_Algoritmo import *
+from cerradura import *
+
 #Funcion reciclada de primeros
 def calcularReglasP(archivo,listaNoTerminales):
     listaProducciones=[]#contiene las tuplas de las producciones
@@ -38,8 +41,16 @@ def calcularReglasP(archivo,listaNoTerminales):
 
     return listaProducciones
 
+def agregarPunto(listaProducciones):
+    listaProduccionesAux = []
+    for produccion in listaProducciones:
+        produccion = (produccion[0], ['•'] + produccion[1])
+        listaProduccionesAux.append(produccion)
+    return listaProduccionesAux
 
-
+def convertirLista(Conjunto):
+    for i, elemento in enumerate(Conjunto):
+        Conjunto[i] = list(elemento)
 
 ##############################################################################################################
 #Abre archivo gramatica.txt
@@ -59,7 +70,111 @@ reglasProduccion=calcularReglasP(archivoGramatica,noTerminales)
 
 
 elemento_gramatica_aumentada = [simboloInicial + "'", [simboloInicial,"$"]]
+
 #Aumentar gramatica
 reglasProduccion.insert(0,elemento_gramatica_aumentada)
+gramatica_aumentada = agregarPunto(reglasProduccion)
+print("\n\nGramatica aumentada: ",gramatica_aumentada)
 
-print(reglasProduccion)
+
+ConjuntoC = cerradura(['I0', [gramatica_aumentada[0]]],reglasProduccion)
+print("\n\nConjunto C: ",ConjuntoC)
+convertirLista(ConjuntoC)
+
+lista_elementos_para_ir_a = [] 
+lista_estados = []
+
+i = 0
+aux_I = [   'I'+ str(i)  , ConjuntoC      ]
+print("\n\naux_I: ", aux_I)
+
+ConjuntoC = []
+ConjuntoC.append(aux_I)
+
+lista_estados_conjuntos = []
+lista_estados_conjuntos.append(ConjuntoC)
+print("\n\nlista_estados_conjuntos: ", lista_estados_conjuntos)
+while (len(ConjuntoC) > 0):
+    for elemento in ConjuntoC: #Vamos a ir recorriendo  
+            #print("elemento: ",elemento)
+            estado = elemento[0]
+            #print("estado: ",estado)
+            #Recorremos el conjunto de producciones generadas por el estado
+            #Obtenemos los simbolos que vamos a evaluar en el ir_a, es decir los simbolos después del punto
+            #print("la regla esta en elemento[1]: ",elemento[1])
+            
+            
+            bandera_se_detecto_punto = False
+            for reglas in elemento[1]:
+                   for e in reglas[1]:
+                    #print("\nelemento de la producción de la regla:", e)
+                    
+                    if bandera_se_detecto_punto:
+                        lista_elementos_para_ir_a.append(e)
+                        bandera_se_detecto_punto = False
+                    
+                    if e == '•':
+                        bandera_se_detecto_punto = True
+                        #print("Se detecto punto")
+
+                
+                    #print("estado de la bandera: ", bandera_se_detecto_punto)
+            #del ConjuntoC[0]                
+                
+    lista_elementos_para_ir_a  = list(set(lista_elementos_para_ir_a))
+    print("Lista de elementos a mandar a Ir_a: ", lista_elementos_para_ir_a)
+
+    numero_estados = len(ConjuntoC)
+    print("numero de estados: ", numero_estados)
+    conta = 0
+    while (conta < numero_estados):
+        for elemento in lista_elementos_para_ir_a:
+            print("ConjuntoC[0]: ")
+            resultado_Ir_a = Ir_a(ConjuntoC[0], elemento, reglasProduccion)
+    conta+=1
+            
+
+
+
+'''
+while (len(conjuntos_C) > 0):
+    for elemento in conjuntos_C: #Vamos a ir recorriendo 
+            print("elemento: ",elemento)
+
+            base  = elemento[0] #Base de la regla de produccion
+            estados = elemento[1] #Estados de la regla de produccion
+            print("estados: ",estados)
+            temporal = [] #Lista temporal para guardar los estados
+            bandera_se_detecto_punto = bandera_se_encontro_simbolo =False
+
+            retorno_Ir_a =[]
+
+            for estado in estados:
+                print("estado: ",estado)
+                for item in estado[1]:
+                    print("item :", item)
+                    
+                    if bandera_se_detecto_punto == True:
+                        print("Se entro a ir a")
+                        retorno_Ir_a = Ir_a(elemento,item)
+
+                        if retorno_Ir_a != None:
+                            #Añadimos a C
+                            for elem_Ir_a in retorno_Ir_a:
+                                #Recorremos C de nuevo para ver que no se repita
+                                for e in conjuntos_C:
+                                    print("elemento: ",elemento)
+                                    b  = elemento[0] #Base de la regla de produccion
+                                    edos = elemento[1] #Estados de la regla de produccion
+                                    print("estados: ",estados)
+                                    temp = [] #Lista temporal para guardar los estados
+                                    if elem_Ir_a in [edo for edo in edos] : 
+                                        conjuntos_C.append(elem_Ir_a)
+                    if estado == '•':
+                        bandera_se_detecto_punto = True
+                        #print("Se detecto punto")
+            del conjuntos_C[0]
+            print("Conjunto C, cada iteracion")
+ '''       
+        
+
