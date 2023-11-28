ab33e50c573fdb67fea2e9de1f26bc6c7d5ce769
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2023-11-28 13:50:33 -0600

    Con coleccion canonica

--- ProyectoCompiladores/Ir_a_Algoritmo.py
+++ ProyectoCompiladores/Ir_a_Algoritmo.py
@@ -11,15 +11,15 @@ def Ir_a(I, simboloAevaluar, reglas_prod, nuevo_elemento_canonica):
     if simboloAevaluar == '$':                     # Si el símbolo a evaluar es el símbolo de fin de cadena
         return "Aceptacion"
     
-    print("Estamos trabajando en ir a")
+    #print("Estamos trabajando en ir a")
     J = []  # Conjunto de elementos vacío
     for elemento in I[1]:
-        print("elemento: ", elemento)
+        #print("elemento: ", elemento)
         base = elemento[0]                          # Obtener el símbolo base de la regla
         produccion = elemento[1]                    # Obtener la producción de la regla
-        print("produccion: ", produccion)
+        #print("produccion: ", produccion)
         for i in range(len(produccion)-1): 
-            print("produccion[i]: ", produccion[i])
+            #print("produccion[i]: ", produccion[i])
             if produccion[i] == '•' and produccion[i + 1] == simboloAevaluar:  # Intercambiar posición con el inmediato siguiente
                 nueva_produccion = produccion.copy()
                 nueva_produccion[i], nueva_produccion[i + 1] = nueva_produccion[i + 1], nueva_produccion[i]
@@ -30,6 +30,6 @@ def Ir_a(I, simboloAevaluar, reglas_prod, nuevo_elemento_canonica):
         return None
     nuevo_elemento_canonica.setEnviadoACerradura(J)      # Agregamos el conjunto J a la tabla de datos para la coleccion canonica, para identificar que se envio a la cerradura
     retornoCerradura = cerradura(['I0',J],reglas_prod)   # Se obtiene el conjunto de elementos resultante de la cerradura
-    print("retornoCerradura: ", retornoCerradura)
+    #print("retornoCerradura: ", retornoCerradura)
     return retornoCerradura                              # Devolver el conjunto de elementos
 
--- ProyectoCompiladores/coleccionCanonica.py
+++ ProyectoCompiladores/coleccionCanonica.py
@@ -1,7 +1,6 @@
 
 from Ir_a_Algoritmo import *
 from cerradura import *
-
 class TablaColeccionCanonica:
     def __init__(self):
         self.estado = []
@@ -44,7 +43,7 @@ class TablaColeccionCanonica:
 
 
 #Funcion reciclada de primeros
-def calcularReglasP(archivo,listaNoTerminales):
+def calcularReglasP(archivo,listaNoTerminales,ruta):
     listaProducciones=[]#contiene las tuplas de las producciones
     cadena = []
     lineas = archivo.readlines()   
@@ -87,7 +86,7 @@ def agregarPunto(listaProducciones):
     for produccion in listaProducciones:
         if produccion[1] == ['λ']:
             produccion = (produccion[0],['•'])
-            print("produccion: ", produccion)
+            #print("produccion: ", produccion)
         else:
             produccion = (produccion[0], ['•'] + produccion[1])
         listaProduccionesAux.append(produccion)
@@ -101,7 +100,7 @@ def convertirLista(Conjunto):
 def buscarEstado(lista_estados_conjuntos, conjunto_ir_a):
     for index, elemento_lista_conjuntos in enumerate(lista_estados_conjuntos):
         if conjunto_ir_a == elemento_lista_conjuntos[1]:
-            print("Ya se encontró este estado en el índice:", index)
+            #print("Ya se encontró este estado en el índice:", index)
             return index
     return -1  # Retorna -1 si no se encontró el estado
 
@@ -111,7 +110,7 @@ def coleccionCanonica(ConjuntoC, reglasProduccion, lista_estados_conjuntos, enum
     while ConjuntoC:
         conjunto_actual = ConjuntoC.pop(0)
 
-        print("conjunto_actual[1]: ", conjunto_actual[1])
+        #print("conjunto_actual[1]: ", conjunto_actual[1])
         for reglas in conjunto_actual[1]:
             regla = reglas[1]
             bandera_se_encontro_estado = -1
@@ -149,7 +148,7 @@ def coleccionCanonica(ConjuntoC, reglasProduccion, lista_estados_conjuntos, enum
                 if (conjunto_ir_a == "Aceptacion"):
                     listaCanonica.append(nuevo_elemento_canonica)
             if bandera_se_encontro_estado != -1:
-                    print("Ya se encontró este estado")
+                    #print("Ya se encontró este estado")
                     nuevo_elemento_canonica.setEstado('I'+str(bandera_se_encontro_estado))
                     listaCanonica.append(nuevo_elemento_canonica)
                     bandera_se_encontro_estado = -1
@@ -161,91 +160,97 @@ def coleccionCanonica(ConjuntoC, reglasProduccion, lista_estados_conjuntos, enum
 
 ##############################################################################################################
 #Abre archivo gramatica.txt
+def main(ruta):
+    #global ruta
 
-ruta="gramatica.txt"
-archivoGramatica=open(ruta,encoding="utf-8")#Usar esta codificacion para que lea lambda
-##Variables
-noTerminales=archivoGramatica.readline().split()
-terminales=archivoGramatica.readline().split()
-simboloInicial = noTerminales[0]
-noTerminales=[]
-terminales=[]
-primerosArray=[]
-reglasProduccion=[]
-reglasProduccion=calcularReglasP(archivoGramatica,noTerminales)
-#print(reglasProduccion)
+    archivoGramatica=open(ruta,encoding="utf-8")#Usar esta codificacion para que lea lambda
+    ##Variables
+    noTerminales=archivoGramatica.readline().split()
+    terminales=archivoGramatica.readline().split()
+    simboloInicial = noTerminales[0]
+    noTerminales=[]
+    terminales=[]
+    primerosArray=[]
+    reglasProduccion=[]
+    reglasProduccion=calcularReglasP(archivoGramatica,noTerminales,ruta)
+    #print(reglasProduccion)
 
 
 
-#Empieza el programa de colección canónica _________________________________________________________________
-elemento_gramatica_aumentada = [simboloInicial + "'", [simboloInicial,"$"]]
-
-#Aumentar gramatica
-reglasProduccion.insert(0,elemento_gramatica_aumentada)
-gramatica_aumentada = agregarPunto(reglasProduccion)
-print("\n\nGramatica aumentada: ",gramatica_aumentada)
-
-reglasProduccion = [regla for regla in reglasProduccion if regla[1] != ['λ']]  #Quitamos las reglas lambda
-# Imprimir las reglas después de eliminar las reglas lambda
-print("Reglas después de eliminar reglas lambda:")
-for regla in reglasProduccion:
-    print(regla)
+    #Empieza el programa de colección canónica _________________________________________________________________
+    elemento_gramatica_aumentada = [simboloInicial + "'", [simboloInicial,"$"]]
+    elemento_gramatica_aumentada_aux = [simboloInicial + "'", [simboloInicial,"$"]] 
+    elemento_gramatica_aumentada_aux[1].insert(0, '•')    
+    print("elemento_gramatica_aumentada_aux: ",elemento_gramatica_aumentada_aux)         
+    print("Enviado:",['I0',[elemento_gramatica_aumentada_aux]])
+    ConjuntoC = cerradura(['I0',[elemento_gramatica_aumentada_aux]],reglasProduccion)
+    print("\n\nConjuntoC: ",ConjuntoC)
     
-#"Preparamos el conjunto C para la coleccion canonica con el elemento inicial"
-#elemento gramatica aumentada, todas las reglas cuya base sea el simbolo inicial y el primer elemento no terminal que se encuentre como produccion de la regla
-ConjuntoC = [['I0',[elemento_gramatica_aumentada]],reglasProduccion]
-#Vamos a buscar la regla asociada a la devuelta para agregarla a C con el punto añadido
-lista_posociones_reglas = []
+    #Aumentar gramatica
+    reglasProduccion.insert(0,elemento_gramatica_aumentada)
+    gramatica_aumentada = agregarPunto(reglasProduccion)
+    print("\n\nGramatica aumentada: ",gramatica_aumentada)
 
-for i, regla in enumerate(reglasProduccion):
-    print("ConjuntoC[1]: ",ConjuntoC[1])
-    for j, elemento in enumerate(ConjuntoC[1]):
-        print("elemento: ",elemento)
-        if elemento == regla: 
-            lista_posociones_reglas.append(i)
-
-        
-print("lista_posociones_reglas: ",lista_posociones_reglas)
-aux = []
-for posicion in lista_posociones_reglas:
-    aux.append(gramatica_aumentada[posicion]) 
-
-i = 0  #Inicializamos el contador de estados, se incrementa cada vez que se crea un nuevo estado
-aux_I = [   'I'+ str(i)  , aux      ] #Creamos el primer estado, el resultante de la cerradura
-
-ConjuntoC.clear()
-ConjuntoC.append(aux_I)
-
-#Nos aseguramos de que todos los elementos interiores sean listas
-for index, elemento in enumerate(ConjuntoC):
-    ConjuntoC[index] = list(elemento)
-
-    for e_index, e in enumerate(elemento[1]):
-        ConjuntoC[index][1][e_index] = list(e)
-      
-#Copiamos en la lista de estados el primer elemento de C
-lista_estados_conjuntos = []
-lista_estados_conjuntos.append(aux_I)
-
-#Convertimos en lista
-for index, elemento in enumerate(ConjuntoC):
-    ConjuntoC[index] = list(elemento)
-
-    for e_index, e in enumerate(elemento[1]):
-        ConjuntoC[index][1][e_index] = list(e)
-
-
-print("\n\nlista_estados_conjuntos: ", lista_estados_conjuntos)
-ResultadosCanonica = coleccionCanonica(ConjuntoC, reglasProduccion, lista_estados_conjuntos,i)
-
-for estado in lista_estados_conjuntos:
-    print("Estado: ", estado)
+    reglasProduccion = [regla for regla in reglasProduccion if regla[1] != ['λ']]  #Quitamos las reglas lambda
     
-Estado_Inicial = TablaColeccionCanonica()
-Estado_Inicial.setEstado(lista_estados_conjuntos[0])
-Estado_Inicial.setEnviadoACerradura(gramatica_aumentada[0])
-ResultadosCanonica.insert(0,Estado_Inicial)
+    #"Preparamos el conjunto C para la coleccion canonica con el elemento inicial"
+    #elemento gramatica aumentada, todas las reglas cuya base sea el simbolo inicial y el primer elemento no terminal que se encuentre como produccion de la regla
+    #ConjuntoC = [['I0',[elemento_gramatica_aumentada]],reglasProduccion]
+        #estadosI0 = []
+        #estadosI0.append(gramatica_aumentada[0])
+        #estadosI0.append(gramatica_aumentada[1])
+        #aux = ['I0',estadosI0]
+        #print("\n\naux: ",aux)
+        #ConjuntoC = cerradura(aux,reglasProduccion)
+        #print("\n\nConjuntoC: ",ConjuntoC)
+    #Vamos a buscar la regla asociada a la devuelta para agregarla a C con el punto añadido
+    lista_posociones_reglas = []
+    #Vamos a buscar la regla asociada a la devuelta para agregarla a C con el punto añadido
+    for i, regla in enumerate(gramatica_aumentada):
+        for j, elemento in enumerate(ConjuntoC):
+            if elemento == list(regla): 
+                lista_posociones_reglas.append(i)
+    print("lista_posociones_reglas: ",lista_posociones_reglas)
+    
+    aux = []
+    for posicion in lista_posociones_reglas:
+        aux.append(gramatica_aumentada[posicion]) 
 
-print("\n\nResultadosCanonica: _______________________________________________________________________________________ ")
-for elemento in ResultadosCanonica:
-    print(elemento)
+    i = 0  #Inicializamos el contador de estados, se incrementa cada vez que se crea un nuevo estado
+    aux_I = [   'I'+ str(i)  , aux      ] #Creamos el primer estado, el resultante de la cerradura
+
+    ConjuntoC.clear()
+    ConjuntoC.append(aux_I)
+
+    #Nos aseguramos de que todos los elementos interiores sean listas
+    for index, elemento in enumerate(ConjuntoC):
+        ConjuntoC[index] = list(elemento)
+
+        for e_index, e in enumerate(elemento[1]):
+            ConjuntoC[index][1][e_index] = list(e)
+
+    #Copiamos en la lista de estados el primer elemento de C
+    lista_estados_conjuntos = []
+    lista_estados_conjuntos.append(aux_I)
+
+    #Convertimos en lista
+    for index, elemento in enumerate(ConjuntoC):
+        ConjuntoC[index] = list(elemento)
+
+        for e_index, e in enumerate(elemento[1]):
+            ConjuntoC[index][1][e_index] = list(e)
+
+
+    ResultadosCanonica = coleccionCanonica(ConjuntoC, reglasProduccion, lista_estados_conjuntos,i)
+
+
+    Estado_Inicial = TablaColeccionCanonica()
+    Estado_Inicial.setEstado(lista_estados_conjuntos[0])
+    Estado_Inicial.setEnviadoACerradura(gramatica_aumentada[0])
+    ResultadosCanonica.insert(0,Estado_Inicial)
+
+    #print("\n\nResultadosCanonica: _______________________________________________________________________________________ ")
+    return ResultadosCanonica#regresa el resultado de la coleccion canonica
+
+
+#main()
