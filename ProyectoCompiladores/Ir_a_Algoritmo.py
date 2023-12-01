aec4992369574d3004388fd5379d05d0c5f86419
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2023-11-30 18:40:34 -0600

    Ya quedo

--- ProyectoCompiladores/Ir_a_Algoritmo.py
+++ ProyectoCompiladores/Ir_a_Algoritmo.py
@@ -7,7 +7,7 @@ def generar_estado(estado, produccion):
 def Ir_a(I, simboloAevaluar, reglas_prod, nuevo_elemento_canonica):
     if len(I) == 0:                                 # Si el conjunto I está vacío
         return None
-    
+        
     if simboloAevaluar == '$':                     # Si el símbolo a evaluar es el símbolo de fin de cadena
         return "Aceptacion"
     
@@ -18,18 +18,31 @@ def Ir_a(I, simboloAevaluar, reglas_prod, nuevo_elemento_canonica):
         base = elemento[0]                          # Obtener el símbolo base de la regla
         produccion = elemento[1]                    # Obtener la producción de la regla
         #print("produccion: ", produccion)
+        
         for i in range(len(produccion)-1): 
-            #print("produccion[i]: ", produccion[i])
+            print("produccion[i]: ", produccion[i])
+            print("produccion", produccion)
+            print("len(produccion)", len(produccion))
+            if produccion[i] == '•' and len(produccion) == 1:
+               print("Se detecta la produccion lambda")
+               nuevo_elemento_canonica.setEnviadoACerradura(produccion)  
+               retornoCerradura = cerradura(['I0',produccion],reglas_prod)  
+               #print("retornoCerradura: ", retornoCerradura)
+               return retornoCerradura  
+            
             if produccion[i] == '•' and produccion[i + 1] == simboloAevaluar:  # Intercambiar posición con el inmediato siguiente
                 nueva_produccion = produccion.copy()
                 nueva_produccion[i], nueva_produccion[i + 1] = nueva_produccion[i + 1], nueva_produccion[i]
+                print(nueva_produccion)
                 new =[base,nueva_produccion]
                 J.append(new)
     
     if len(J) == 0:                                      #El conjunto J está vacío
         return None
+    print("J agregando:", J)
     nuevo_elemento_canonica.setEnviadoACerradura(J)      # Agregamos el conjunto J a la tabla de datos para la coleccion canonica, para identificar que se envio a la cerradura
+    #print(nuevo_elemento_canonica)
     retornoCerradura = cerradura(['I0',J],reglas_prod)   # Se obtiene el conjunto de elementos resultante de la cerradura
-    #print("retornoCerradura: ", retornoCerradura)
+    print("retornoCerradura: ", retornoCerradura)
     return retornoCerradura                              # Devolver el conjunto de elementos
 
--- ProyectoCompiladores/coleccionCanonica.py
+++ ProyectoCompiladores/coleccionCanonica.py
@@ -36,11 +36,19 @@ class TablaColeccionCanonica:
         self.estadoIr_A = estadoIr_A
     
     def setEnviadoACerradura(self, EnviadoACerradura):
-        self.EnviadoACerradura = EnviadoACerradura
+        if self.EnviadoACerradura is None:
+            self.EnviadoACerradura = EnviadoACerradura
+        else:
+            self.EnviadoACerradura.extend(EnviadoACerradura)
     
     def __str__(self):
         return "\nEstado: " + str(self.estado) + " \nSimboloIr_A: " + str(self.SimboloIr_A) + " \nEstadoIr_A: " + str(self.estadoIr_A) + " \nEnviadoACerradura: " + str(self.EnviadoACerradura) + "\n"
 
+    def clear(self):
+        self.estado = []
+        self.SimboloIr_A = '-'
+        self.estadoIr_A = '-'
+        self.EnviadoACerradura = []
 
 #Funcion reciclada de primeros
 def calcularReglasP(archivo,listaNoTerminales,ruta):
@@ -108,53 +116,73 @@ def buscarEstado(lista_estados_conjuntos, conjunto_ir_a):
             return index
     return -1  # Retorna -1 si no se encontró el estado
 
+def quitarLambda(ConjuntoC):
+    for i,elem in enumerate(ConjuntoC):
+        for j, produccion in enumerate(elem):
+            if produccion[j] == 'λ':
+                ConjuntoC[i][j].remove('λ')
+
 def coleccionCanonica(ConjuntoC, reglasProduccion, lista_estados_conjuntos, enumeracion_estados):
     listaCanonica = []
     
     while ConjuntoC:
         conjunto_actual = ConjuntoC.pop(0)
-
-        #print("conjunto_actual[1]: ", conjunto_actual[1])
+        print("conjunto_actual[1]: ", conjunto_actual[1])
+        caracteresEvaluados = []
+        
+        conjunto_ir_a = []
         for reglas in conjunto_actual[1]:
             regla = reglas[1]
             bandera_se_encontro_estado = -1
-            conjunto_ir_a = []
+            bandera_no_se_ha_calculado_transicion = -1
             for i in range(len(regla)): 
+                print("Regla para ir a", regla)
                 if regla[len(regla)-1] == '•' :
                     conjunto_ir_a = None
-                
+                #if regla[i] == '•' and regla[i+1] != None and regla[i+1] != 'λ':
                 elif regla[i] == '•' and regla[i+1] != None:
-                    #Creamos el objeto que contiene la informacion sobre el proceso que se esta haciendo
-                    nuevo_elemento_canonica = TablaColeccionCanonica()
-                    nuevo_elemento_canonica.setEstadoIr_A(conjunto_actual[0])
-                    nuevo_elemento_canonica.setSimboloIr_A(regla[i+1])
-                    
-                    #Llamamos  ala funcion Ir_a que internamente llama a la funcion cerradura
-                    conjunto_ir_a = Ir_a(conjunto_actual, regla[i+1], reglasProduccion, nuevo_elemento_canonica)
-                    
-                    bandera_se_encontro_estado = -1
-                    bandera_se_encontro_estado = buscarEstado(lista_estados_conjuntos, conjunto_ir_a)
+                    print ("regla [i+1]" ,regla[i+1] )
+                    print(caracteresEvaluados)
+                    if regla[i+1] not in caracteresEvaluados:
+                        #Creamos el objeto que contiene la informacion sobre el proceso que se esta haciendo
+                        nuevo_elemento_canonica = TablaColeccionCanonica()
+                        nuevo_elemento_canonica.setEstadoIr_A(conjunto_actual[0])
+                        nuevo_elemento_canonica.setSimboloIr_A(regla[i+1])
+
+                        simbolo_evaluar_ir_a = regla[i+1]
+                        caracteresEvaluados.append(regla[i+1])
+                        
+                        #Llamamos  ala funcion Ir_a que internamente llama a la funcion cerradura
+                        conjunto_ir_a = Ir_a(conjunto_actual, simbolo_evaluar_ir_a, reglasProduccion, nuevo_elemento_canonica)
+                        print("Conjunto ir a para quitar lamda", conjunto_ir_a )
+                        if conjunto_ir_a != None:
+                            quitarLambda(conjunto_ir_a)
+                        bandera_se_encontro_estado = -1
+                        bandera_no_se_ha_calculado_transicion = 1
+                        bandera_se_encontro_estado = buscarEstado(lista_estados_conjuntos, conjunto_ir_a)
 
 
             #Evaluamos si la bandera de busqueda de estado            
                 
-            if bandera_se_encontro_estado == -1:
+            if bandera_se_encontro_estado == -1 and bandera_no_se_ha_calculado_transicion == 1:
                 if (conjunto_ir_a != None and conjunto_ir_a != "Aceptacion"):
                     enumeracion_estados += 1
                     nuevo_estado = ['I' + str(enumeracion_estados), conjunto_ir_a]
                     #Insertamos en la informacion para la tabla
                     nuevo_elemento_canonica.setEstado(nuevo_estado)
                     listaCanonica.append(nuevo_elemento_canonica)
+                    #nuevo_elemento_canonica.clear()
                     #Guardamos la recopilacion de todos los estados
                     lista_estados_conjuntos.append(nuevo_estado)
                     ConjuntoC.append(nuevo_estado)
                     
                 if (conjunto_ir_a == "Aceptacion"):
                     listaCanonica.append(nuevo_elemento_canonica)
-            if bandera_se_encontro_estado != -1:
+            if bandera_se_encontro_estado != -1 and bandera_no_se_ha_calculado_transicion == 1:
                     #print("Ya se encontró este estado")
                     nuevo_elemento_canonica.setEstado('I'+str(bandera_se_encontro_estado))
                     listaCanonica.append(nuevo_elemento_canonica)
+                    #nuevo_elemento_canonica.clear()
                     bandera_se_encontro_estado = -1
  
     return listaCanonica           
@@ -187,26 +215,22 @@ def main(ruta):
     elemento_gramatica_aumentada_aux[1].insert(0, '•')    
     print("elemento_gramatica_aumentada_aux: ",elemento_gramatica_aumentada_aux)         
     print("Enviado:",['I0',[elemento_gramatica_aumentada_aux]])
+    print(reglasProduccion)
     ConjuntoC = cerradura(['I0',[elemento_gramatica_aumentada_aux]],reglasProduccion)
     print("\n\nConjuntoC: ",ConjuntoC)
+    
+    quitarLambda(ConjuntoC)
+    print("\n\nConjuntoC post quitado de lambda: ",ConjuntoC)
+    
     
     #Aumentar gramatica
     reglasProduccion.insert(0,elemento_gramatica_aumentada)
     gramatica_aumentada = agregarPunto(reglasProduccion)
     print("\n\nGramatica aumentada: ",gramatica_aumentada)
 
-    reglasProduccion = [regla for regla in reglasProduccion if regla[1] != ['λ']]  #Quitamos las reglas lambda
+    #reglasProduccion = [regla for regla in reglasProduccion if regla[1] != ['λ']]  #Quitamos las reglas lambda
     
-    #"Preparamos el conjunto C para la coleccion canonica con el elemento inicial"
-    #elemento gramatica aumentada, todas las reglas cuya base sea el simbolo inicial y el primer elemento no terminal que se encuentre como produccion de la regla
-    #ConjuntoC = [['I0',[elemento_gramatica_aumentada]],reglasProduccion]
-        #estadosI0 = []
-        #estadosI0.append(gramatica_aumentada[0])
-        #estadosI0.append(gramatica_aumentada[1])
-        #aux = ['I0',estadosI0]
-        #print("\n\naux: ",aux)
-        #ConjuntoC = cerradura(aux,reglasProduccion)
-        #print("\n\nConjuntoC: ",ConjuntoC)
+    
     #Vamos a buscar la regla asociada a la devuelta para agregarla a C con el punto añadido
     lista_posociones_reglas = []
 
