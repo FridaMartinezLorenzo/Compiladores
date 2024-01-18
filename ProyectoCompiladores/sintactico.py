7e79346c47ead7c5ef5904504e9ea5ca650aef11
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2024-01-17 20:03:38 -0600

    Reacomodando

--- ProyectoCompiladores/sintactico.py
+++ ProyectoCompiladores/sintactico.py
@@ -6,10 +6,9 @@ from interfazPyS import *
 from InterfazCanonica import *
 from TablaAnalisisSintactico import *
 from analisisSintacticoLR import *
-from Analizador_Sintactico import *
 
 def DesplegarAlgoritmoSintactico(opcion):
-    arr=["Primeros y siguientes","Colección canónica","Tabla Analisis Sintactico","Análisis Sintáctico LR", "Analizador Sintactico"]
+    arr=["Primeros y siguientes","Colección canónica","Tabla Analisis Sintactico","Análisis Sintáctico LR"]
     if opcion==arr[0]:
         interfazPyS()
     elif opcion==arr[1]:
@@ -18,9 +17,6 @@ def DesplegarAlgoritmoSintactico(opcion):
         InterfazTablaAS()
     elif opcion == arr[3]:
         analisisSintactico()
-    elif opcion == arr[4]:
-        analizadorSintacticoJava()
-        
     else:
         messagebox.showerror("Error","Aún no existe esa opción")
     return
