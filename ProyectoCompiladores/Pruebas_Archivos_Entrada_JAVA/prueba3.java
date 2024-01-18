2de11e2b646c0cf0fca7036430b1ac61460074ce
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2024-01-18 15:09:10 -0600

    Se agrego la posivilidad de crear metodos estaticos

--- ProyectoCompiladores/Pruebas_Archivos_Entrada_JAVA/prueba3.java
+++ ProyectoCompiladores/Pruebas_Archivos_Entrada_JAVA/prueba3.java
@@ -1,5 +1,12 @@
+
 public class prueba3 {
     int suma(string[] args) {
        int x, y;
+       float z = 1.1;
+       int a = 1;
     }
+
+}
+class Animal {
+
 }
---
+++ ProyectoCompiladores/Pruebas_Archivos_Entrada_JAVA/prueba4.java
@@ -0,0 +1,8 @@
+public class prueba4 {
+    public static void main(string[] args) {
+       int x, y;
+       float z = 1.1;
+       int a = 1;
+    }
+
+}
