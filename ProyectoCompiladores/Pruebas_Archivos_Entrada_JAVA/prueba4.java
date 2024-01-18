9dce251600e266df624a0ff6d497c6cfec9903b9
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2024-01-18 15:15:41 -0600

    Variables multiples con asignacion

--- ProyectoCompiladores/Pruebas_Archivos_Entrada_JAVA/prueba4.java
+++ ProyectoCompiladores/Pruebas_Archivos_Entrada_JAVA/prueba4.java
@@ -1,7 +1,7 @@
 public class prueba4 {
     public static void main(string[] args) {
        int x, y;
-       float z = 1.1;
+       float z = 1.1, k;
        int a = 1;
        public static int count = 0;
     }
